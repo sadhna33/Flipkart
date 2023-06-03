7baccbe7a66ff413ad76338429e55c9591b5994e
sadhna <sadhnajethuri1@gmail.com>
2023-06-03 14:30:44 +0530

    req

--- customer/urls.py
+++ customer/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,include
 from customer.views import *
 
 urlpatterns = [
-    path('get-customer',GetCustomerViews.as_view())
-    
+    path('get-customer',GetCustomerViews.as_view()),
+    path('get-address',GetCustomerViews.as_view()),
+    path('get-address',CustomerDetailAddressView.as_view()),
 
 ]
